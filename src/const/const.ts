64b65bd87c572bd5a7c2935bcc18db75180d76e8
한병국 <hanhanhan333@gmail.com>
2022-03-25 12:23:07 +0900

    refac: 스토어 이동 로직, 스토어 크기 조절
    1. 스토어 이동 좌표값을, 시작좌표와 드랍 좌표로 계산
    2. 스토어 크기 조절

---
+++ src/const/const.ts
@@ -0,0 +1,7 @@
+const STORE_SIZE = {
+  s: 100,
+  m: 150,
+  l: 200,
+};
+
+export { STORE_SIZE };
--- src/page/admin/Admin.tsx
+++ src/page/admin/Admin.tsx
@@ -1,29 +1,47 @@
 import BackgroundUploader from 'components/admin/BackgroundUploader';
-import Store from 'components/admin/Store';
+import { STORE_SIZE } from 'const/const';
 import React, { ChangeEventHandler, MouseEventHandler, useRef, useState } from 'react';
 import style from './Admin.module.scss';
 
 const Admin = () => {
   const ref = useRef(null);
-  const [storeName, setStoreName] = useState('');
+  const [newStoreName, setNewStoreName] = useState('');
   const [src, setSrc] = useState('');
   const [imgSize, setImgSize] = useState({ width: 0, height: 0 });
-  const [stores, setStores] = useState<{ [propName: string]: number[] | undefined }>({});
+  const [stores, setStores] = useState<{
+    [propName: string]: { coodinate: number[]; size: 's' | 'm' | 'l' } | undefined;
+  }>({});
   const [isChangingStore, setIsChangingStore] = useState('');
+  const [startCoodinate, setStartCoodinate] = useState([0, 0]);
 
   const preventDefaultHandler = (e: React.SyntheticEvent) => e.preventDefault();
 
   const setStoreNameHandler: ChangeEventHandler<HTMLInputElement> = (e) =>
-    setStoreName(e.currentTarget.value);
+    setNewStoreName(e.currentTarget.value);
 
   const setStoreHandler: MouseEventHandler<HTMLButtonElement> = () => {
-    if (!storeName.length) return alert('매장 이름을 입력해주세요.');
-    else if (stores[storeName]) return alert('이미 등록된 매장입니다.');
-    else setStores((pre) => ({ ...pre, [storeName]: [0, 0] }));
+    if (!newStoreName.length) return alert('매장 이름을 입력해주세요.');
+    else if (stores[newStoreName]) return alert('이미 등록된 매장입니다.');
+    else setStores((pre) => ({ ...pre, [newStoreName]: { coodinate: [0, 0], size: 'm' } }));
   };
 
-  const setCoodinateHandler: MouseEventHandler = ({ nativeEvent: { offsetX, offsetY } }) => {
-    setStores((pre) => ({ ...pre, [isChangingStore]: [offsetX - 50, offsetY - 50] }));
+  const setCoodinateHandler: MouseEventHandler = (e) => {
+    const { pageX, pageY } = e;
+    setStores((pre) => ({
+      ...pre,
+      [isChangingStore]: {
+        coodinate: [
+          pre[isChangingStore]!.coodinate[0] + (pageX - startCoodinate[0]),
+          pre[isChangingStore]!.coodinate[1] + (pageY - startCoodinate[1]),
+        ],
+        size: pre[isChangingStore]!.size,
+      },
+    }));
+  };
+
+  const setStartCoodinateHandler: MouseEventHandler = (e) => {
+    const { pageX, pageY } = e;
+    setStartCoodinate([pageX, pageY]);
   };
 
   return (
@@ -31,7 +49,7 @@ const Admin = () => {
       <BackgroundUploader setBackgroundSrc={setSrc} setBackgroundSize={setImgSize} />
       {src && (
         <>
-          <input type="text" value={storeName} onChange={setStoreNameHandler} />
+          <input type="text" value={newStoreName} onChange={setStoreNameHandler} />
           <button onClick={setStoreHandler}>매장 추가</button>
           <div
             className={style.box}
@@ -44,25 +62,70 @@ const Admin = () => {
               alt="매장 구조도"
               ref={ref}
               onDragOver={preventDefaultHandler}
-              onDrop={setCoodinateHandler}
+              onDrop={(e) => {
+                setCoodinateHandler(e);
+                setIsChangingStore('');
+              }}
             />
             {Object.keys(stores).length
               ? Object.keys(stores).map((store) => {
                   if (!stores[store]) return null;
                   return (
-                    <Store
-                      stores={stores}
-                      setStores={setStores}
-                      storeName={storeName}
-                      setIsChangingStore={setIsChangingStore}
+                    <div
+                      draggable
+                      className={style.store}
                       key={store}
-                    />
+                      style={{
+                        width: STORE_SIZE[stores[store]!.size],
+                        height: STORE_SIZE[stores[store]!.size],
+                        left: stores[store]!.coodinate[0],
+                        top: stores[store]!.coodinate[1],
+                        backgroundColor: isChangingStore === store ? 'yellow' : 'transparent',
+                      }}
+                      onDragStart={(e) => {
+                        setStartCoodinateHandler(e);
+                        setIsChangingStore(store);
+                      }}
+                    >
+                      <div>
+                        {store}
+                        <button
+                          onClick={() => setStores((pre) => ({ ...pre, [store]: undefined }))}
+                        >
+                          삭제
+                        </button>
+                        <select
+                          defaultValue={'m'}
+                          onChange={(e) => {
+                            const { value } = e.currentTarget;
+                            setStores((pre) => ({
+                              ...pre,
+                              [store]: {
+                                coodinate: [...pre[store]!.coodinate],
+                                size: value as 's' | 'm' | 'l',
+                              },
+                            }));
+                          }}
+                        >
+                          <option value={'s'}>s</option>
+                          <option value={'m'}>m</option>
+                          <option value={'l'}>l</option>
+                        </select>
+                      </div>
+                    </div>
                   );
                 })
               : null}
           </div>
         </>
       )}
+      <button
+        onClick={() => {
+          localStorage.setItem('stores', JSON.stringify(stores));
+        }}
+      >
+        저장
+      </button>
     </>
   );
 };
