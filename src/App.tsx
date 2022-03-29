0613122dc7e27ca9af3ac920b00d3c9e971c52fb
한병국 <hanhanhan333@gmail.com>
2022-03-29 13:41:39 +0900

    feat: page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import Admin from 'page/admin/Admin';
+import { useState } from 'react';
 
 function App() {
-  return <Admin />;
+  const [page, setPage] = useState<'admin' | 'user'>('admin');
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setPage('admin')}>ADMIN</button>
+        <button onClick={() => setPage('user')}>USER</button>
+      </div>
+      {page === 'admin' ? <Admin /> : null}
+    </>
+  );
 }
 
 export default App;
