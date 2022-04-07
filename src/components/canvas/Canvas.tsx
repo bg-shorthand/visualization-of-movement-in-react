1b89ed4367dbe67e30abbf59b0ba11e3c9acb939
나윤호 <>
2022-04-07 16:49:50 +0900

    [추가] 스토어 클릭 시 해당 스토어와 관련된 화살표만 표출

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,15 +1,29 @@
 import { mockData } from 'assets/mockData';
 import storeImg from 'assets/store-sample-img.png';
 import { drawArrow } from 'modules/drawArrow';
-import { useEffect, useRef, useState } from 'react';
+import React, { MouseEventHandler, useEffect, useRef, useState } from 'react';
+import styles from './Canvas.module.scss';
+
+interface Store {
+  name: string;
+  coodinate: number[];
+}
+type Stores = Store[];
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const store = mockData.store;
+  const stores = mockData.store as Stores;
 
   const [canvasSize, setCanvasSize] = useState({ width: 0, height: 0 });
+  const [selectedStores, setSelectedStores] = useState<Stores>([]);
+
+  const storeSelectedClick: MouseEventHandler<HTMLLIElement> = (e) => {
+    const { id } = e.currentTarget;
+    setSelectedStores((pre) => [...pre, stores.find((store) => store.name === id)!]);
+  };
 
   useEffect(() => {
+    console.log(selectedStores);
     const canvas = canvasRef.current;
     if (!canvas) return;
     const ctx = canvas.getContext('2d');
@@ -20,49 +34,85 @@ const Canvas = () => {
       const { width, height } = img;
       setCanvasSize({ width, height });
       ctx.drawImage(img, 0, 0);
-      store.forEach((v, i) => {
-        store.forEach((w, j) => {
-          if (i <= j) return;
+
+      if (selectedStores.length === 1) {
+        stores.forEach((v) => {
+          if (v.name === selectedStores[0].name) return;
           drawArrow(
-            storeImg,
             ctx,
+            selectedStores[0].coodinate[0],
+            selectedStores[0].coodinate[1],
             v.coodinate[0],
             v.coodinate[1],
-            w.coodinate[0],
-            w.coodinate[1],
-            4,
-            8,
-            false,
-            true,
+            7,
+            10,
             2,
             true
           );
           drawArrow(
-            storeImg,
             ctx,
-            w.coodinate[0],
-            w.coodinate[1],
             v.coodinate[0],
             v.coodinate[1],
-            4,
-            8,
-            false,
-            true,
+            selectedStores[0].coodinate[0],
+            selectedStores[0].coodinate[1],
+            7,
+            10,
             2,
             false
           );
         });
-      });
-      // drawArrow(storeImg, ctx, 797, 152, 1147, 132, 4, 8, false, true, 2, false); // test
-      // drawArrow(storeImg, ctx, 1147, 332, 597, 352, 4, 8, false, true, 2, false);
+      } else {
+        stores.forEach((v, i) => {
+          stores.forEach((w, j) => {
+            if (i <= j) return;
+            drawArrow(
+              ctx,
+              v.coodinate[0],
+              v.coodinate[1],
+              w.coodinate[0],
+              w.coodinate[1],
+              7,
+              10,
+              2,
+              true
+            );
+            drawArrow(
+              ctx,
+              w.coodinate[0],
+              w.coodinate[1],
+              v.coodinate[0],
+              v.coodinate[1],
+              7,
+              10,
+              2,
+              false
+            );
+          });
+        });
+      }
     };
     img.src = storeImg;
-  }, [store]);
+  }, [selectedStores, stores]);
 
   return (
-    <>
+    <div className={styles.canvasWrapper}>
       <canvas ref={canvasRef} width={canvasSize.width} height={canvasSize.height}></canvas>
-    </>
+      <ul>
+        {stores.map((item) => {
+          return (
+            <li
+              id={item.name}
+              className={styles.storeList}
+              key={item.coodinate[0]}
+              onClick={storeSelectedClick}
+              style={{ left: `${item.coodinate[0] - 50}px`, top: `${item.coodinate[1] - 50}px` }}
+            >
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
--- src/modules/drawArrow.ts
+++ src/modules/drawArrow.ts
@@ -1,5 +1,4 @@
 export function drawArrow(
-  storeImg: string,
   ctx: CanvasRenderingContext2D,
   x0: number, // 시작점 x
   y0: number, // 시작점 y
@@ -7,8 +6,6 @@ export function drawArrow(
   y1: number, // 끝점 y
   aWidth: number, // 화살촉이 선에서 수직으로 연장되는 거리
   aLength: number, // 화살 날개의 길이
-  arrowStart: boolean, // 시작점에 화살촉 유무 결정
-  arrowEnd: boolean, // 끝점에 화살촉 유무 결정
   lineWidth: number, // 선의 전체 굵기
   secondary: boolean
 ) {
@@ -16,70 +13,40 @@ export function drawArrow(
   const dy = y1 - y0;
   const angle = Math.atan2(dy, dx);
   const length = Math.sqrt(dx * dx + dy * dy);
-  const arrowWholeLength = 30;
-  const arcLength = 50;
+  const arrowWholeLength = 45;
+  const arrowGap = 35;
+  const arcLength = arrowGap + 50;
+  const curveAlgle = 30;
 
   if (ctx) {
     ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = 'blue';
-    ctx.fillStyle = 'blue';
-    ctx.translate(x0, y0); // 원점에서 다른 점으로 이동
-    ctx.rotate(angle); // canvas를 현재의 원점 둘레도 회전
+    ctx.translate(x0, y0);
+    ctx.rotate(angle);
 
-    ctx.beginPath(); // 하위 경로 초기화, 새로 그리기
-    // ctx.moveTo(0, 0); // 시작점, 뒷부분 길이 줄일 때
-    // ctx.lineTo(length - 30, 0); // 시작점과 moveTo의 원점을 연결하여 선 생성 -> 결국 length가 선의 길이 결정, 앞부분 길이 줄일 때
-    if (!secondary) {
-      ctx.moveTo(arrowWholeLength, 0);
-      // ctx.lineTo(length - arrowWholeLength, 0); // 직선
-      ctx.bezierCurveTo(
-        arrowWholeLength,
-        arcLength,
-        length - arrowWholeLength,
-        arcLength,
-        length - arrowWholeLength,
-        0
-      ); // 커브
-      if (arrowEnd) {
-        ctx.moveTo(length - arrowWholeLength - aLength + 10, -aWidth + 10);
-        ctx.lineTo(length - arrowWholeLength, 0);
-        ctx.lineTo(length - arrowWholeLength - aLength + 2, aWidth);
-        ctx.closePath();
-      }
-    } else {
-      ctx.strokeStyle = 'white';
-      ctx.moveTo(arrowWholeLength, 0);
-      // ctx.lineTo(length - arrowWholeLength, 10); // 직선
-      ctx.bezierCurveTo(
-        arrowWholeLength,
-        arcLength,
-        length - arrowWholeLength,
-        arcLength,
-        length - arrowWholeLength,
-        0
-      ); // 커브
+    // Line
+    ctx.beginPath();
+    ctx.strokeStyle = secondary ? 'white' : 'blue';
+    ctx.fillStyle = secondary ? 'white' : 'blue';
+    ctx.moveTo(arrowWholeLength, arrowGap);
+    ctx.bezierCurveTo(
+      length / 2 - curveAlgle,
+      arcLength,
+      length / 2 + curveAlgle,
+      arcLength,
+      length - arrowWholeLength,
+      arrowGap
+    );
 
-      if (arrowEnd) {
-        ctx.moveTo(length - arrowWholeLength - aLength + 10, -aWidth + 10);
-        ctx.lineTo(length - arrowWholeLength, 0);
-        ctx.lineTo(length - arrowWholeLength - aLength + 2, aWidth);
-        ctx.closePath();
-      }
-    }
-
-    // if (arrowStart) {
-    //   ctx.moveTo(aLength, -aWidth);
-    //   ctx.lineTo(0, 0);
-    //   ctx.lineTo(aLength, aWidth);
-    // }
-
-    // if (arrowEnd) {
-    //   ctx.moveTo(length - arrowWholeLength - aLength, -aWidth);
-    //   ctx.lineTo(length - arrowWholeLength, 0);
-    //   ctx.lineTo(length - arrowWholeLength - aLength, aWidth);
-    // }
-
+    // Arrow Header
     ctx.stroke();
+    ctx.translate(length - arrowWholeLength + 2, -aWidth + arrowGap);
+    ctx.rotate((Math.PI / 180) * 120);
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(aLength, aWidth);
+    ctx.lineTo(aLength, -aWidth);
+    ctx.fill();
+
     ctx.setTransform(1, 0, 0, 1, 0, 0);
   }
 }
