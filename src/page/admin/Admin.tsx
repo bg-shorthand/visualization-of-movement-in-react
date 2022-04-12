52a0aa64fbdfa011082175550322e65383c628bf
한병국 <hanhanhan333@gmail.com>
2022-04-12 13:51:40 +0900

    refac: mock data type

--- src/page/admin/Admin.tsx
+++ src/page/admin/Admin.tsx
@@ -1,16 +1,13 @@
 import BackgroundUploader from 'components/admin/BackgroundUploader';
-import { STORE_SIZE } from 'const/const';
-import React, { ChangeEventHandler, MouseEventHandler, useRef, useState } from 'react';
+import { Stores } from 'const/type';
+import React, { ChangeEventHandler, MouseEventHandler, useState } from 'react';
 import style from './Admin.module.scss';
 
 const Admin = () => {
-  const ref = useRef(null);
   const [newStoreName, setNewStoreName] = useState('');
   const [src, setSrc] = useState('');
   const [imgSize, setImgSize] = useState({ width: 0, height: 0 });
-  const [stores, setStores] = useState<{
-    [propName: string]: { coodinate: number[]; size: 's' | 'm' | 'l' } | undefined;
-  }>({});
+  const [stores, setStores] = useState<Stores>([]);
   const [isChangingStore, setIsChangingStore] = useState('');
   const [startCoodinate, setStartCoodinate] = useState([0, 0]);
 
@@ -21,22 +18,26 @@ const Admin = () => {
 
   const setStoreHandler: MouseEventHandler<HTMLButtonElement> = () => {
     if (!newStoreName.length) return alert('매장 이름을 입력해주세요.');
-    else if (stores[newStoreName]) return alert('이미 등록된 매장입니다.');
-    else setStores((pre) => ({ ...pre, [newStoreName]: { coodinate: [0, 0], size: 'm' } }));
+    else if (stores.find((store) => store.name === newStoreName))
+      return alert('이미 등록된 매장입니다.');
+    else setStores((pre) => [...pre, { name: newStoreName, coodinate: [0, 0] }]);
   };
 
   const setCoodinateHandler: MouseEventHandler = (e) => {
     const { pageX, pageY } = e;
-    setStores((pre) => ({
-      ...pre,
-      [isChangingStore]: {
-        coodinate: [
-          pre[isChangingStore]!.coodinate[0] + (pageX - startCoodinate[0]),
-          pre[isChangingStore]!.coodinate[1] + (pageY - startCoodinate[1]),
-        ],
-        size: pre[isChangingStore]!.size,
-      },
-    }));
+    setStores((pre) =>
+      pre.map(({ name, coodinate }) =>
+        name === isChangingStore
+          ? {
+              name,
+              coodinate: [
+                coodinate[0] + (pageX - startCoodinate[0]),
+                coodinate[1] + (pageY - startCoodinate[1]),
+              ],
+            }
+          : { name, coodinate }
+      )
+    );
   };
 
   const setStartCoodinateHandler: MouseEventHandler = (e) => {
@@ -60,30 +61,28 @@ const Admin = () => {
               className={style.img}
               src={src}
               alt="매장 구조도"
-              ref={ref}
               onDragOver={preventDefaultHandler}
               onDrop={(e) => {
                 setCoodinateHandler(e);
               }}
             />
-            {Object.keys(stores).length
-              ? Object.keys(stores).map((store) => {
-                  if (!stores[store]) return null;
+            {stores.length
+              ? stores.map(({ name, coodinate }) => {
                   return (
                     <div
                       draggable
                       className={style.store}
-                      key={store}
+                      key={name}
                       style={{
-                        width: STORE_SIZE[stores[store]!.size],
-                        height: STORE_SIZE[stores[store]!.size],
-                        left: stores[store]!.coodinate[0],
-                        top: stores[store]!.coodinate[1],
-                        backgroundColor: isChangingStore === store ? 'yellow' : 'transparent',
+                        width: '100px',
+                        height: '100px',
+                        left: coodinate[0],
+                        top: coodinate[1],
+                        backgroundColor: name === isChangingStore ? 'yellow' : 'transparent',
                       }}
                       onDragStart={(e) => {
                         setStartCoodinateHandler(e);
-                        setIsChangingStore(store);
+                        setIsChangingStore(name);
                       }}
                       onDragEnd={() => setIsChangingStore('')}
                       onDragOver={preventDefaultHandler}
@@ -92,29 +91,14 @@ const Admin = () => {
                       }}
                     >
                       <div>
-                        {store}
+                        {name}
                         <button
-                          onClick={() => setStores((pre) => ({ ...pre, [store]: undefined }))}
+                          onClick={() =>
+                            setStores((pre) => pre.filter((store) => store.name !== name))
+                          }
                         >
                           삭제
                         </button>
-                        <select
-                          defaultValue={'m'}
-                          onChange={(e) => {
-                            const { value } = e.currentTarget;
-                            setStores((pre) => ({
-                              ...pre,
-                              [store]: {
-                                coodinate: [...pre[store]!.coodinate],
-                                size: value as 's' | 'm' | 'l',
-                              },
-                            }));
-                          }}
-                        >
-                          <option value={'s'}>s</option>
-                          <option value={'m'}>m</option>
-                          <option value={'l'}>l</option>
-                        </select>
                       </div>
                     </div>
                   );
